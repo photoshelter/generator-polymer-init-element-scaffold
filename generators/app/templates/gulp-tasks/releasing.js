ab8333826115e54872c6500711179600bae2d9c1
caranicas <kendrick.caranicas@gmail.com>
2017-04-11 14:54:00 -0400

    testable working version

--- generators/app/templates/gulp-tasks/releasing.js
+++ generators/app/templates/gulp-tasks/releasing.js
@@ -3,46 +3,26 @@ var bump = require('gulp-bump');
 var git  = require('gulp-git');
 var filter = require('gulp-filter');
 var tag = require('gulp-tag-version');
-var push = require('gulp-git-push');
+var savefile = require('gulp-savefile');
 
 function newRelease(importance) {
     return gulp.src(['./package.json', './bower.json'])
         .pipe(bump(
             {type:importance}
         ))
-        .pipe(gulp.dest('./'))
-        // commit the changed files
+        .pipe(savefile())
         .pipe(git.commit('bump version'))
-        // filter one file
         .pipe(filter('package.json'))
-        // create tag based on the filtered file
         .pipe(tag())
-        // push changes into repository
-        .pipe(push({                      
-            repository: 'origin',
-            refspec: 'HEAD'
-        }));
 };
 
 gulp.task('tag-patch', function () { return newRelease('patch'); });
 gulp.task('tag-minor', function () { return newRelease('minor'); });
 gulp.task('tag-major', function () { return newRelease('major'); });
 
-gulp.task('reset-cache', function () {
- return gulp.src(['./package.json', './bower.json'])
-    .pipe(git.rm({args:'--cached'}));
-});
-
-gulp.task('commit-json', function () {
-   return gulp.src(['./package.json', './bower.json'])
-        .pipe(git.commit('bump version'));
-});
-
-gulp.task('version-push', function(done){
-  git.push('origin', 'master', function (err) {
+gulp.task('push-tag', function(done){
+  git.push('origin', 'master' , {args: " --tags"}, function (err) {
     if (err) throw err;
+    done();
   });
-  done();
 });
-
-gulp.task('commit-bump', gulp.series('reset-cache', 'commit-json', 'version-push'));
--- generators/app/templates/gulpfile.js
+++ generators/app/templates/gulpfile.js
@@ -10,10 +10,3 @@ gulp.task('default', gulp.series('copy', 'serve'));
  * GitHub Pages Demos 
 **/
 gulp.task('pages-deployment', gulp.series('copy', 'pages'));
-
-/**
- * Tagging Releases 
-**/
-gulp.task('release-patch', gulp.series('tag-patch', 'commit-bump'));
-gulp.task('release-minor', gulp.series('tag-minor', 'commit-bump'));
-gulp.task('release-major', gulp.series('tag-major', 'commit-bump'));
