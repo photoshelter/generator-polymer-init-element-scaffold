4b4792f6e3ae42f51bdd2355cb9e9623971f5b2f
Eric Olivas <eric@photoshelter.com>
2018-05-31 12:22:16 -0400

    Updated travis.sh for headless testing & updated test script.

--- generators/app/templates/travis-scripts/_travis.sh
+++ generators/app/templates/travis-scripts/_travis.sh
@@ -1,18 +1,14 @@
-
 #! /bin/bash
+set -ev
 
 GIT_REPO_BASEDIR=`basename $(git rev-parse --show-toplevel)`
 BUILD_NUMBER=${TRAVIS_BUILD_NUMBER:-LOCALHOST}
 JOB_NAME="${GIT_REPO_BASEDIR}-${BUILD_NUMBER}"
 TUNNEL_IDENTIFIER="${TRAVIS_JOB_NUMBER}"
 
-echo "begin testing on saucelabs"
-echo "GIT REPO BASEDIR: ${GIT_REPO_BASEDIR}"
-echo "BUILD_NUMBER: ${BUILD_NUMBER}"
-echo "JOB_NAME: ${JOB_NAME}"
-echo "TUNNEL_IDENTIFIER: ${TUNNEL_IDENTIFIER}"
-
-wct --plugin sauce \
+if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then  
+  echo 'Running in Saucelabs';
+  wct --plugin sauce \
   --sauce "OSX 10.13/chrome@latest" \
   --sauce "OSX 10.13/firefox@latest" \
   --sauce "OSX 10.13/safari@latest" \
@@ -23,6 +19,14 @@ wct --plugin sauce \
   --skip-plugin local \
   --job-name $JOB_NAME \
   --build-number $BUILD_NUMBER \
-  --visibility team
+  --visibility team;
+fi
 
-  echo "tests complete"
+# If this is not a Pull Request we will run our tests on 
+# We don't always want to run against saucelabs when we do a push
+# This allows us to test headlessly against Chrome and firefox.
+# 
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+  echo 'Running headless.';
+  xvfb-run -e /dev/stdout -a wct;
+fi
